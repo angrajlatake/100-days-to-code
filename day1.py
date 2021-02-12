72144b72eee02a8de50bdaddffe360ad9aab10b0
angrajlatake <angraj.latake@gmail.com>
2021-02-12 15:01:16 +0530

    another exercise

--- day1.py
+++ day1.py
@@ -1,1 +1,26 @@
+"""#print command
 print("Hello World!")
+name = input("What's your name? ")  # setup a variable
+# you can setup a value to variable and change it later as well e.g
+a = 21
+print(a)
+a = 99
+print(a)
+
+print("Hello " + name)  # this way dont have to write another line for input
+print("Welcome to the world of\nCODING!")  # this is to move word coding to the next line
+print(len(name))  # find length of the object
+"""
+#switch values from one variable to another
+
+a = input("a:")
+b = input("b:")
+
+# CREATE ONE MORE VARIABLE TO ASSIGN THE FIRST VALUE
+
+c = a
+a = b
+b = c
+
+print("a: " + a + "\nb: " + b )
+
