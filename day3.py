7a888f7569ec13d4f1138841d9d9165e66ba8d83
angrajlatake <angraj.latake@gmail.com>
2021-02-12 21:21:20 +0530

    first

---
+++ day3.py
@@ -0,0 +1,9 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? \n"))
+if height >= 120 :
+    print("You can ride the rollercoaster!")
+
+else:
+    print("Sorry, You have to grow taller before you ride.")
+
+
