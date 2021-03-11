e2143890282cf40e7960227bf03c8a0b6b60cfe1
angrajlatake <angraj.latake@gmail.com>
2021-03-11 18:55:35 +0530

    day 13 done

---
+++ day13.py
@@ -0,0 +1,35 @@
+# #find the bug in the following program
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+
+#find the bug in the following program
+
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
+
+#find the bug in the following program
+
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
