51faa0b0e886ee602c94dade45c51d29046f6959
angrajlatake <angraj.latake@gmail.com>
2021-05-04 23:08:37 +0530

    updated high score saving file

--- day-23 Turtle Crossing game/scoreboard.py
+++ day-23 Turtle Crossing game/scoreboard.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 
 
+
 FONT = ("Courier", 24, "normal")
 
 
@@ -15,16 +16,25 @@ class Score(Turtle):
         self.color("black")
         self.hideturtle()
         self.score = 0
-
+        with open("high_score.txt") as file:
+            self.high_score = int(file.read())
         self.update_score()
 
     def update_score(self):
         self.clear()
-        self.write(f"Score {self.score} ", align="center", font = FONT)
+        self.write(f"Score {self.score}  High Score : {self.high_score} ", align="center", font = FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font= FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("high_score.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align="center", font= FONT)
 
     def add_point(self):
         self.score += 1
