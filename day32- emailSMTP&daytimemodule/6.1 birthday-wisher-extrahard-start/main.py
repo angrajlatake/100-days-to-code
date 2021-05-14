2a47292860c4c7c7e244344ac622fbac75a1486d
angrajlatake <angraj.latake@gmail.com>
2021-05-15 01:19:21 +0530

    day 31 done

---
+++ day32- emailSMTP&daytimemodule/6.1 birthday-wisher-extrahard-start/main.py
@@ -0,0 +1,38 @@
+##################### Extra Hard Starting Project ######################
+import random
+import smtplib
+
+import pandas as pd
+import datetime as dt
+# 1. Update the birthdays.csv
+data = pd.read_csv("birthdays.csv")
+
+# 2. Check if today matches a birthday in the birthdays.csv
+now = dt.datetime.now()
+day_tuple = (now.day, now.month)
+new_dict = {(row.day, row.month): row for index, row in data.iterrows()}
+if day_tuple in new_dict:
+    name = new_dict[day_tuple]["name"]
+    email = new_dict[day_tuple]["email"]
+    print(email)
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+    letter1 = "./letter_templates/letter_1.txt"
+    letter2 = "./letter_templates/letter_2.txt"
+    letter3 = "./letter_templates/letter_3.txt"
+    letters_list = [letter3,letter1,letter2]
+    chosen_letter = random.choice(letters_list)
+    placeholder = "[NAME]"
+    with open(chosen_letter, mode="r") as letter:
+        content = letter.read()
+        new_content = content.replace(placeholder, name)
+    print(new_content)
+
+# 4. Send the letter generated in step 3 to that person's email address.
+with smtplib.SMTP("smtp.gmail.com", port= 587) as connection:
+    connection.starttls()
+    connection.login(user="lataketest@gmail.com", password="password")#replace the correct password
+    connection.sendmail(from_addr="lataketest@gmail.com", to_addrs=email, msg=f"Subject: Happy Birthday\n\n {new_content}")
+
+
+
+
---
+++ day32- emailSMTP&daytimemodule/main.py
@@ -0,0 +1,49 @@
+#this code is giving Unicode encode error
+'''import datetime as dt
+import random
+
+import smtplib
+
+
+if dt.datetime.weekday(dt.datetime.now()) == 4:
+    with open("quotes.txt") as file:
+        list = file.readlines()
+        quote = (list[random.randint(0,102)])
+
+print(uni(quote))
+
+my_email = "lataketest@gmail.com"
+password = "Qwerty@987"
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password= password)
+    connection.sendmail(from_addr=my_email,to_addrs=my_email, msg=f"Subject:Monday Motivation Test\n\n {quote}")
+'''
+
+
+#Monday Motivation Project
+
+import smtplib
+import datetime as dt
+import random
+
+MY_EMAIL = "lataketest@gmail.com"
+MY_PASSWORD = "password" #replace the correct password
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 4:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+print(quote)
+
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    connection.login(MY_EMAIL, MY_PASSWORD)
+    connection.sendmail(
+        from_addr=MY_EMAIL,
+        to_addrs=MY_EMAIL,
+        msg=f"Subject:Monday Motivation\n\n{quote}"
+    )
